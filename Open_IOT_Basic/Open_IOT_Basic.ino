b11d0b33c42f55b95b82c24cb216546a2e88da10
joeybab3 <joey_babcock@yahoo.com>
2020-06-14 20:53:43 -0700

    Add basic sketch

---
+++ Open_IOT_Basic/Open_IOT_Basic.ino
@@ -0,0 +1,12 @@
+// Open IOT
+// Control/interface with you Electric Scooter
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
