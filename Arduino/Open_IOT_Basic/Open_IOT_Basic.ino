e386ba5e44d6a5ac66d17b3a159ba43a1eb06d2f
Joey Babcock <joeybab3@users.noreply.github.com>
2020-11-10 10:35:54 -0800

    Add my url

--- Arduino/Open_IOT_Basic/Open_IOT_Basic.ino
+++ Arduino/Open_IOT_Basic/Open_IOT_Basic.ino
@@ -2,7 +2,7 @@
 // Control/interface with your Electric Scooter
 // Standalone version, does not require a backend
 // https://github.com/joeybab3/Open-IOT
-
+// https://joeybabcock.me/blog/electric_scooters/open-iot/
 void setup() {
   // put your setup code here, to run once:
 
